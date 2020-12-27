053e75a58b28a75d54342ef9d8e58c4b4b6d007e
Pablo Diaz <pabloandi@gmail.com>
2020-12-27 17:41:10 -0500

    setting data and asserting styles lesson completed

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
         <span class="count" v-text="count"></span>
-        <button @click="count++"></button>
+        <button class="increment" @click="count++">Increment</button>
+        <button class="decrement" @click="count--" v-show="count > 0">Decrement</button>
     </div>
 </template>
 
--- tests/unit/counter.spec.js
+++ tests/unit/counter.spec.js
@@ -5,19 +5,40 @@ import expect from 'expect';
 describe('Counter', () => {
     
     let wrapper; 
+    let vm;
 
     beforeEach(() => {
         wrapper = mount(Counter);
+        vm = wrapper.vm;
     });
 
      it('defaults to a count of 0', () => {
         expect(wrapper.vm.count).toBe(0);
      });
 
-     it('increments the count when the button is clicked', () => {
+     it('increments the count when the increment button is clicked', () => {
         expect(wrapper.vm.count).toBe(0);
-        wrapper.find('button').trigger('click');
+        wrapper.find('.increment').trigger('click');
         expect(wrapper.vm.count).toBe(1);
+     });
+
+     it('decrements the count when the decrement button is clicked', () => {
+        wrapper.setData({
+            count: 5
+        });
+        wrapper.find('.decrement').trigger('click');
+        expect(wrapper.vm.count).toBe(4);
+     });
+
+     it('never show the decrement button when counter is 0', async () => {
+        expect(wrapper.vm.count).toBe(0);
+        expect(wrapper.find('.decrement').isVisible()).toBe(false);
+        await wrapper.setData({
+            count: 5
+        });
+        expect(wrapper.vm.count).toBe(5);
+            
+        expect(wrapper.find('.decrement').isVisible()).toBe(true);
      });
 
      it('presents the current count', () => {
